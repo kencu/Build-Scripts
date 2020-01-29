a8e4fe5cf894c73ecac30a1de74ce5f31e391c79
Jeffrey Walton <noloader@gmail.com>
2020-01-29 17:54:28 -0500

    Add MPFR recipe

---
+++ build-mpfr.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds GNU MPFR from sources.
+
+MPFR_XZ=mpfr-4.0.2.tar.xz
+MPFR_TAR=mpfr-4.0.2.tar
+MPFR_DIR=mpfr-4.0.2
+PKG_NAME=mpfr
+
+###############################################################################
+
+CURR_DIR=$(pwd)
+function finish {
+  cd "$CURR_DIR" || exit 1
+}
+trap finish EXIT
+
+# Sets the number of make jobs if not set in environment
+: "${INSTX_JOBS:=4}"
+
+###############################################################################
+
+# Get the environment as needed. We can't export it because it includes arrays.
+if ! source ./setup-environ.sh
+then
+    echo "Failed to set environment"
+    exit 1
+fi
+
+if [[ -e "$INSTX_CACHE/$PKG_NAME" ]]; then
+    # Already installed, return success
+    echo ""
+    echo "$PKG_NAME is already installed."
+    exit 0
+fi
+
+# The password should die when this subshell goes out of scope
+if [[ -z "$SUDO_PASSWORD" ]]; then
+    source ./setup-password.sh
+fi
+
+###############################################################################
+
+if ! ./build-cacert.sh
+then
+    echo "Failed to install CA Certs"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-iconv-gettext.sh
+then
+    echo "Failed to build iConv and GetText"
+    exit 1
+fi
+
+###############################################################################
+
+echo
+echo "********** GNU MPFR **********"
+echo
+
+if ! "$WGET" -O "$MPFR_XZ" --ca-certificate="$LETS_ENCRYPT_ROOT" \
+     "https://ftp.gnu.org/gnu/mpfr/$MPFR_XZ"
+then
+    echo "Failed to download GNU MPFR"
+    exit 1
+fi
+
+rm -rf "$MPFR_TAR" "$MPFR_DIR" &>/dev/null
+unxz "$MPFR_XZ" && tar -xf "$MPFR_TAR"
+cd "$MPFR_DIR" || exit 1
+
+# Fix sys_lib_dlsearch_path_spec and keep the file time in the past
+../fix-config.sh
+
+    PKG_CONFIG_PATH="${BUILD_PKGCONFIG[*]}" \
+    CPPFLAGS="${BUILD_CPPFLAGS[*]}" \
+    CFLAGS="${BUILD_CFLAGS[*]}" \
+    CXXFLAGS="${BUILD_CXXFLAGS[*]}" \
+    LDFLAGS="${BUILD_LDFLAGS[*]}" \
+    LIBS="${BUILD_LIBS[*]}" \
+./configure \
+    --prefix="$INSTX_PREFIX" \
+    --libdir="$INSTX_LIBDIR" \
+    --enable-shared \
+    --enable-static
+
+if [[ "$?" -ne 0 ]]; then
+    echo "Failed to configure GNU MPFR"
+    exit 1
+fi
+
+echo "**********************"
+echo "Building package"
+echo "**********************"
+
+MAKE_FLAGS=("-j" "$INSTX_JOBS" "V=1")
+if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build GNU MPFR"
+    exit 1
+fi
+
+echo "**********************"
+echo "Testing package"
+echo "**********************"
+
+MAKE_FLAGS=("check" "V=1")
+if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to test GNU MPFR"
+    exit 1
+fi
+
+echo "Searching for errors hidden in log files"
+COUNT=$(find . -name '*.log' -exec grep -o 'runtime error:' {} \; | wc -l)
+if [[ "${COUNT}" -ne 0 ]];
+then
+    echo "Failed to test GNU MPFR"
+    exit 1
+fi
+
+echo "**********************"
+echo "Installing package"
+echo "**********************"
+
+MAKE_FLAGS=("install")
+if [[ -n "$SUDO_PASSWORD" ]]; then
+    echo "$SUDO_PASSWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd "$CURR_DIR" || exit 1
+
+# Set package status to installed. Delete the file to rebuild the package.
+touch "$INSTX_CACHE/$PKG_NAME"
+
+###############################################################################
+
+echo ""
+echo "*****************************************************************************"
+echo "Please run Bash's 'hash -r' to update program cache in the current shell"
+echo "*****************************************************************************"
+
+###############################################################################
+
+# Set to false to retain artifacts
+if false; then
+
+    ARTIFACTS=("$MPFR_XZ" "$MPFR_TAR" "$MPFR_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-mpfr.sh 2>&1 | tee build-mpfr.log
+    if [[ -e build-mpfr.log ]]; then
+        rm -f build-mpfr.log
+    fi
+fi
+
+exit 0
