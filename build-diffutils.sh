dcc5c6c55b748f3614c889c8cc81e53b318919bc
Jeffrey Walton <noloader@gmail.com>
2020-02-03 21:38:07 -0500

    Add Diffutils recipe

---
+++ build-diffutils.sh
@@ -0,0 +1,154 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds GNU Diffutils from sources.
+
+DIFFUTILS_XZ=diffutils-3.7.tar.xz
+DIFFUTILS_TAR=diffutils-3.7.tar
+DIFFUTILS_DIR=diffutils-3.7
+
+###############################################################################
+
+CURR_DIR=$(pwd)
+function finish {
+    cd "$CURR_DIR" || exit 1
+}
+trap finish EXIT
+
+# Sets the number of make jobs if not set in environment
+: "${INSTX_JOBS:=4}"
+
+###############################################################################
+
+# Get the environment as needed. We can't export it because it includes arrays.
+if ! source ./setup-environ.sh
+then
+    echo "Failed to set environment"
+    exit 1
+fi
+
+# The password should die when this subshell goes out of scope
+if [[ -z "$SUDO_PASSWORD" ]]; then
+    source ./setup-password.sh
+fi
+
+###############################################################################
+
+if ! ./build-cacert.sh
+then
+    echo "Failed to install CA Certs"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-iconv-gettext.sh
+then
+    echo "Failed to build iConv and GetText"
+    exit 1
+fi
+
+###############################################################################
+
+echo
+echo "********** GNU Diffutils **********"
+echo
+
+if ! "$WGET" -O "$DIFFUTILS_XZ" --ca-certificate="$LETS_ENCRYPT_ROOT" \
+     "https://ftp.gnu.org/gnu/diffutils/$DIFFUTILS_XZ"
+then
+    echo "Failed to download GNU Diffutils"
+    exit 1
+fi
+
+rm -rf "$DIFFUTILS_TAR" "$DIFFUTILS_DIR" &>/dev/null
+unxz "$DIFFUTILS_XZ" && tar -xf "$DIFFUTILS_TAR"
+cd "$DIFFUTILS_DIR" || exit 1
+
+# Fix sys_lib_dlsearch_path_spec and keep the file time in the past
+../fix-config.sh
+
+    PKG_CONFIG_PATH="${BUILD_PKGCONFIG[*]}" \
+    CPPFLAGS="${BUILD_CPPFLAGS[*]}" \
+    CFLAGS="${BUILD_CFLAGS[*]}" \
+    CXXFLAGS="${BUILD_CXXFLAGS[*]}" \
+    LDFLAGS="${BUILD_LDFLAGS[*]}" \
+    LIBS="${BUILD_LIBS[*]}" \
+./configure \
+    --prefix="$INSTX_PREFIX" \
+    --libdir="$INSTX_LIBDIR" \
+    --with-libiconv-prefix="$INSTX_PREFIX" \
+    --with-libintl-prefix="$INSTX_PREFIX"
+
+if [[ "$?" -ne 0 ]]; then
+    echo "Failed to configure GNU Diffutils"
+    exit 1
+fi
+
+echo "**********************"
+echo "Building package"
+echo "**********************"
+
+MAKE_FLAGS=("-j" "$INSTX_JOBS" "V=1")
+if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build GNU Diffutils"
+    exit 1
+fi
+
+echo "**********************"
+echo "Testing package"
+echo "**********************"
+
+MAKE_FLAGS=("check" "V=1")
+if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to test GNU Diffutils"
+    exit 1
+fi
+
+echo "Searching for errors hidden in log files"
+COUNT=$(find . -name '*.log' ! -name 'config.log' -exec grep -o 'runtime error:' {} \; | wc -l)
+if [[ "${COUNT}" -ne 0 ]];
+then
+    echo "Failed to test GNU Diffutils"
+    exit 1
+fi
+
+echo "**********************"
+echo "Installing package"
+echo "**********************"
+
+MAKE_FLAGS=("install")
+if [[ -n "$SUDO_PASSWORD" ]]; then
+    echo "$SUDO_PASSWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd "$CURR_DIR" || exit 1
+
+###############################################################################
+
+echo ""
+echo "*****************************************************************************"
+echo "Please run Bash's 'hash -r' to update program cache in the current shell"
+echo "*****************************************************************************"
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true; then
+
+    ARTIFACTS=("$DIFFUTILS_XZ" "$DIFFUTILS_TAR" "$DIFFUTILS_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-awk.sh 2>&1 | tee build-awk.log
+    if [[ -e build-awk.log ]]; then
+        rm -f build-awk.log
+    fi
+fi
+
+exit 0
