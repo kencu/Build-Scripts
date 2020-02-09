ce105228395bcfb5407dace79b65bbc1da7202b7
Jeffrey Walton <noloader@gmail.com>
2020-02-09 00:31:24 -0500

    Add fix-pc recipe

---
+++ fix-pc.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# This script fixes *.pc files. It removes extra fodder rom Libs and
+# Libs.private. It is needed because some configure scripts cannot
+# handle the extra options in pkg config files. For example, Zile
+# fails to find Ncurses because Ncurses uses the following in its
+# *.pc file:
+#     Libs: -L<path> -Wl,-rpath,<path> -lncurses -ltinfo
+# Zile can find the libraries when using:
+#     Libs: -L<path> -lncurses -ltinfo
+
+if [[ -n "$1" ]]; then
+    PROG_PATH="$1"
+elif [[ -d "./programs" ]]; then
+    PROG_PATH="./programs"
+elif [[ -d "../programs" ]]; then
+    PROG_PATH="../programs"
+elif [[ -d "../../programs" ]]; then
+    PROG_PATH="../../programs"
+elif [[ -d "../../../programs" ]]; then
+    PROG_PATH="../../../programs"
+fi
+
+if ! "${CXX}" "$PROG_PATH/fix-pc.cpp" -o fix-pc.exe;
+then
+    echo "Failed to build fix-pc"
+    exit 1
+fi
+
+(IFS="\r\n" find "$PWD" -iname '*.pc' -print | while read -r file
+do
+    echo "Fixing $file..."
+    cp -p "$file" "$file.fixed"
+    ./fix-pc.exe "$file" > "$file.fixed"
+    mv "$file.fixed" "$file"
+done)
+
+exit 0
---
+++ programs/fix-pc.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+void usage_exit();
+void process_stream(std::istream&);
+std::string fix_lib(const std::string&);
+
+int main(int argc, char* argv[])
+{
+    if (argc > 2)
+        usage_exit();
+
+    if (argc == 1)
+    {
+        process_stream(std::cin);
+    }
+    else
+    {
+        std::ifstream infile (argv[1]);
+        process_stream(infile);
+    }
+
+    return 0;
+}
+
+void process_stream(std::istream& stream)
+{
+    std::string line;
+    while (std::getline(stream, line))
+    {
+        std::cout << fix_lib(line) << std::endl;
+    }
+}
+
+std::string fix_lib(const std::string& line)
+{
+    std::string new_line;
+
+    if (line.substr(0, 5) == "Libs:")
+        new_line = "Libs:";
+    else if (line.substr(0, 13) == "Libs.private:")
+        new_line = "Libs.private:";
+    else
+        return line;
+
+    std::string t;
+    std::istringstream stream(line);
+
+    while (getline(stream, t, ' '))
+    {
+        if (t.empty())
+            continue;
+
+        if (t.substr(0,2) == "-l")
+            new_line += std::string(" ") + t;
+        else if (t.substr(0,2) == "-L")
+            new_line += std::string(" ") + t;
+    }
+
+    return new_line;
+}
+
+void usage_exit()
+{
+    std::cerr << "Usage: fix_pc <pc_file>" << std::endl;
+    std::cerr << "   or: cat <pc_file> | fix_pc" << std::endl;
+    exit(1);
+}
