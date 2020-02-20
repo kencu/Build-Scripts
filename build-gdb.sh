3679eea1277aa784ed24b7fff64c5584c1384678
Jeffrey Walton <noloader@gmail.com>
2020-02-20 17:06:33 -0500

    Add GDB recipe

---
+++ build-gdb.sh
@@ -0,0 +1,184 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds GDB from sources.
+
+GDB_XZ=gdb-9.1.tar.xz
+GDB_TAR=gdb-9.1.tar
+GDB_DIR=gdb-9.1
+
+###############################################################################
+
+CURR_DIR=$(pwd)
+function finish {
+    cd "$CURR_DIR" || exit 1
+}
+trap finish EXIT
+
+# Sets the number of make jobs if not set in environment
+: "${INSTX_JOBS:=4}"
+
+###############################################################################
+
+# Get the environment as needed. We can't export it because it includes arrays.
+if ! source ./setup-environ.sh
+then
+    echo "Failed to set environment"
+    exit 1
+fi
+
+# The password should die when this subshell goes out of scope
+if [[ -z "$SUDO_PASSWORD" ]]; then
+    source ./setup-password.sh
+fi
+
+###############################################################################
+
+if ! ./build-cacert.sh
+then
+    echo "Failed to install CA Certs"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-mpfr.sh
+then
+    echo "Failed to build MPFR"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-gmp.sh
+then
+    echo "Failed to build GMP"
+    exit 1
+fi
+
+###############################################################################
+
+echo
+echo "********** GDB **********"
+echo
+
+if ! "$WGET" -O "$GDB_XZ" --ca-certificate="$LETS_ENCRYPT_ROOT" \
+     "https://ftp.gnu.org/gnu/gdb/$GDB_XZ"
+then
+    echo "Failed to download GDB"
+    exit 1
+fi
+
+rm -rf "$GDB_TAR" "$GDB_DIR" &>/dev/null
+unxz "$GDB_XZ" && tar -xf "$GDB_TAR"
+cd "$GDB_DIR" || exit 1
+
+# Patches are created with 'diff -u' from the pkg root directory.
+if [[ -e ../patch/gdb.patch ]]; then
+    cp ../patch/gdb.patch .
+    patch -u -p0 < gdb.patch
+    echo ""
+fi
+
+# Fix sys_lib_dlsearch_path_spec and keep the file time in the past
+cp -p ../fix-config.sh .; ./fix-config.sh
+
+# GDB must be built in a directory different from its sources
+mkdir -p build || exit 1
+cd build || exit 1
+
+CONFIG_OPTS=()
+CONFIG_OPTS+=("--disable-lto")
+CONFIG_OPTS+=("--with-mpfr=$INSTX_PREFIX")
+CONFIG_OPTS+=("--with-gmp=$INSTX_PREFIX")
+
+    PKG_CONFIG_PATH="${BUILD_PKGCONFIG[*]}" \
+    CPPFLAGS="${BUILD_CPPFLAGS[*]}" \
+    CFLAGS="${BUILD_CFLAGS[*]}" \
+    CXXFLAGS="${BUILD_CXXFLAGS[*]}" \
+    LDFLAGS="${BUILD_LDFLAGS[*]}" \
+    LIBS="${BUILD_LIBS[*]}" \
+../configure \
+    --prefix="$INSTX_PREFIX" \
+    --libdir="$INSTX_LIBDIR" \
+    "${CONFIG_OPTS[@]}"
+
+if [[ "$?" -ne 0 ]]; then
+    echo "Failed to configure GDB"
+    exit 1
+fi
+
+echo "**********************"
+echo "Building package"
+echo "**********************"
+
+MAKE_FLAGS=("MAKEINFO=true" "HELP2MAN=true" "-j" "$INSTX_JOBS" "V=1")
+if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build GDB"
+    exit 1
+fi
+
+# Fix flags in *.pc files
+cp -p ../fix-pc.sh .; ./fix-pc.sh
+
+echo "**********************"
+echo "Testing package"
+echo "**********************"
+
+MAKE_FLAGS=("check" "V=1")
+if ! "$MAKE" "${MAKE_FLAGS[@]}"
+then
+    echo "**********************"
+    echo "Failed to test GDB"
+    echo "**********************"
+    exit 1
+fi
+
+echo "Searching for errors hidden in log files"
+COUNT=$(find . -name '*.log' ! -name 'config.log' -exec grep -o 'runtime error:' {} \; | wc -l)
+if [[ "${COUNT}" -ne 0 ]];
+then
+    echo "**********************"
+    echo "Failed to test GDB"
+    echo "**********************"
+    exit 1
+fi
+
+echo "**********************"
+echo "Installing package"
+echo "**********************"
+
+MAKE_FLAGS=("install")
+if [[ -n "$SUDO_PASSWORD" ]]; then
+    echo "$SUDO_PASSWORD" | sudo -S "$MAKE" "${MAKE_FLAGS[@]}"
+else
+    "$MAKE" "${MAKE_FLAGS[@]}"
+fi
+
+cd "$CURR_DIR" || exit 1
+
+###############################################################################
+
+echo ""
+echo "*****************************************************************************"
+echo "Please run Bash's 'hash -r' to update program cache in the current shell"
+echo "*****************************************************************************"
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true; then
+
+    ARTIFACTS=("$GDB_XZ" "$GDB_TAR" "$GDB_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-gdb.sh 2>&1 | tee build-gdb.log
+    if [[ -e build-gdb.log ]]; then
+        rm -f build-gdb.log
+    fi
+fi
+
+exit 0
