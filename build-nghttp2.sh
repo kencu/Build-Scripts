4a1c2cfdb25cb5770eadbc3be0c0fd7501aee78c
Jeffrey Walton <noloader@gmail.com>
2020-03-23 21:58:15 -0400

    Fix NGHTTP2 recipe

--- build-nghttp2.sh
+++ build-nghttp2.sh
@@ -86,7 +86,7 @@ echo "**********************"
 echo "Downloading package"
 echo "**********************"
 
-if ! "$WGET" -q -O "$NGHTTP2_TAR" --ca-certificate="$LETS_ENCRYPT_ROOT" \
+if ! "$WGET" -q -O "$NGHTTP2_TAR" --ca-certificate="$GITHUB_ROOT" \
      "https://github.com/nghttp2/nghttp2/releases/download/v$NGHTTP2_VER/$NGHTTP2_TAR"
 then
     echo "Failed to download NGHTTP2"
