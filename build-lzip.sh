7b4e441d66c833557d3c808e62e45bdce1602fd3
Jeffrey Walton <noloader@gmail.com>
2020-03-30 02:59:06 -0400

    Update Lzip recipe

--- build-lzip.sh
+++ build-lzip.sh
@@ -110,7 +110,7 @@ echo "Testing package"
 echo "**********************"
 
 MAKE_FLAGS=("check")
-if ! ${MAKE} "${MAKE_FLAGS[@]}"
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
 then
     echo "**********************"
     echo "Failed to test Lzip"
@@ -134,9 +134,9 @@ echo "**********************"
 
 MAKE_FLAGS=("install")
 if [[ -n "$SUDO_PASSWORD" ]]; then
-    printf "%s\n" "$SUDO_PASSWORD" | sudo -E -S ${MAKE} "${MAKE_FLAGS[@]}"
+    printf "%s\n" "$SUDO_PASSWORD" | sudo -E -S "${MAKE}" "${MAKE_FLAGS[@]}"
 else
-    ${MAKE} "${MAKE_FLAGS[@]}"
+    "${MAKE}" "${MAKE_FLAGS[@]}"
 fi
 
 cd "$CURR_DIR" || exit 1
