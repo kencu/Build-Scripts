4702b41c44b6f3dc5614207e6966019272490727
Jeffrey Walton <noloader@gmail.com>
2020-04-01 23:12:28 -0400

    Update fix-sudo.sh script

--- fix-sudo.sh
+++ fix-sudo.sh
@@ -10,9 +10,14 @@ do
         continue
     fi
 
-    chmod a+w "$file"
-    cp -p "$file" "$file.fixed"
+	# 'cp -p' copies all permissions and timestamps
+	# https://pubs.opengroup.org/onlinepubs/9699919799/utilities/cp.html
+    chmod u+w "$file" && cp -p "$file" "$file.fixed"
     sed 's/sudo -E/sudo/g' "$file" > "$file.fixed"
     mv "$file.fixed" "$file"
-    chmod a+x "$file" && chmod o-w "$file"
 done)
+
+echo "The -E option has been removed from sudo. This may cause some"
+echo "unexpected results, especially for packages that build stuff"
+echo "during 'sudo make install'. For example, Perl will create a"
+echo ".cpan folder in the user's home directory owned by root."
