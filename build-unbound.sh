03a2c36ee7f4a99f30d6899ecb9dd7e83c89b7a3
Jeffrey Walton <noloader@gmail.com>
2020-05-19 04:26:13 -0400

    Bump Unbound version

--- build-unbound.sh
+++ build-unbound.sh
@@ -3,8 +3,8 @@
 # Written and placed in public domain by Jeffrey Walton
 # This script builds Unbound from sources.
 
-UNBOUND_TAR=unbound-1.10.0.tar.gz
-UNBOUND_DIR=unbound-1.10.0
+UNBOUND_TAR=unbound-1.10.1.tar.gz
+UNBOUND_DIR=unbound-1.10.1
 PKG_NAME=unbound
 
 ###############################################################################
