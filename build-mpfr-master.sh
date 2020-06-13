9cacac46ad5c034a39f74b13e3792c993326ad31
Jeffrey Walton <noloader@gmail.com>
2020-06-13 11:17:36 -0400

    Add MPFR master script

---
+++ build-mpfr-master.sh
@@ -0,0 +1,184 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds MPFR from sources.
+
+MPFR_TAR=mpfr-4.1.0-rc1.tar.gz
+MPFR_DIR=mpfr-4.1.0-rc1
+PKG_NAME=mpfr
+
+###############################################################################
+
+CURR_DIR=$(pwd)
+function finish {
+    cd "$CURR_DIR" || exit 1
+}
+trap finish EXIT INT
+
+# Sets the number of make jobs if not set in environment
+: "${INSTX_JOBS:=2}"
+
+###############################################################################
+
+# Get the environment as needed. We can't export it because it includes arrays.
+if ! source ./setup-environ.sh
+then
+    echo "Failed to set environment"
+    exit 1
+fi
+
+if [[ -e "$INSTX_PKG_CACHE/$PKG_NAME" ]]; then
+    echo ""
+    echo "$PKG_NAME is already installed."
+    exit 0
+fi
+
+# The password should die when this subshell goes out of scope
+if [[ "$SUDO_PASSWORD_SET" != "yes" ]]; then
+    if ! source ./setup-password.sh
+    then
+        echo "Failed to process password"
+        exit 1
+    fi
+fi
+
+###############################################################################
+
+if ! ./build-cacert.sh
+then
+    echo "Failed to install CA Certs"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-iconv-gettext.sh
+then
+    echo "Failed to build iConv and GetText"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-gmp.sh
+then
+    echo "Failed to build GMP"
+    exit 1
+fi
+
+###############################################################################
+
+echo
+echo "********** MPFR **********"
+echo
+
+echo "**********************"
+echo "Downloading package"
+echo "**********************"
+
+if ! "$WGET" -q -O "$MPFR_TAR" --ca-certificate="$CA_ZOO" \
+     "https://www.mpfr.org/mpfr-4.1.0/$MPFR_TAR"
+then
+    echo "Failed to download MPFR"
+    exit 1
+fi
+
+rm -rf "$MPFR_DIR" &>/dev/null
+gzip -d < "$MPFR_TAR" | tar xf -
+cd "$MPFR_DIR" || exit 1
+
+if false; then
+
+# Per INSTALL
+if "$WGET" -q -O allpatches --ca-certificate="$CA_ZOO" \
+     https://www.mpfr.org/mpfr-4.0.2/allpatches
+then
+    patch -N -Z -p1 < allpatches
+else
+    echo "Failed to download MPFR patches"
+    exit 1
+fi
+
+fi
+
+# Fix sys_lib_dlsearch_path_spec
+bash ../fix-configure.sh
+
+    PKG_CONFIG_PATH="${BUILD_PKGCONFIG[*]}" \
+    CPPFLAGS="${BUILD_CPPFLAGS[*]}" \
+    CFLAGS="${BUILD_CFLAGS[*]}" \
+    CXXFLAGS="${BUILD_CXXFLAGS[*]}" \
+    LDFLAGS="${BUILD_LDFLAGS[*]}" \
+    LIBS="${BUILD_LIBS[*]}" \
+./configure \
+    --build="$AUTOCONF_BUILD" \
+    --prefix="$INSTX_PREFIX" \
+    --libdir="$INSTX_LIBDIR" \
+    --enable-shared \
+    --enable-static \
+    --with-gmp-include="$INSTX_PREFIX/include" \
+    --with-gmp-lib="$INSTX_LIBDIR"
+
+if [[ "$?" -ne 0 ]]; then
+    echo "Failed to configure MPFR"
+    exit 1
+fi
+
+echo "**********************"
+echo "Building package"
+echo "**********************"
+
+MAKE_FLAGS=("-j" "$INSTX_JOBS" "V=1")
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build MPFR"
+    exit 1
+fi
+
+# Fix flags in *.pc files
+bash ../fix-pkgconfig.sh
+
+echo "**********************"
+echo "Testing package"
+echo "**********************"
+
+MAKE_FLAGS=("check" "V=1")
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to test MPFR"
+    exit 1
+fi
+
+echo "**********************"
+echo "Installing package"
+echo "**********************"
+
+MAKE_FLAGS=("install")
+if [[ -n "$SUDO_PASSWORD" ]]; then
+    printf "%s\n" "$SUDO_PASSWORD" | sudo -E -S "${MAKE}" "${MAKE_FLAGS[@]}"
+else
+    "${MAKE}" "${MAKE_FLAGS[@]}"
+fi
+
+cd "$CURR_DIR" || exit 1
+
+# Set package status to installed. Delete the file to rebuild the package.
+touch "$INSTX_PKG_CACHE/$PKG_NAME"
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true; then
+
+    ARTIFACTS=("$MPFR_TAR" "$MPFR_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-mpfr.sh 2>&1 | tee build-mpfr.log
+    if [[ -e build-mpfr.log ]]; then
+        rm -f build-mpfr.log
+    fi
+fi
+
+exit 0
