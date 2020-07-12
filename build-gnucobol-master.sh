62d55fb8236751e31ae534be90c62301a78d5c19
Jeffrey Walton <noloader@gmail.com>
2020-07-12 07:04:56 -0400

    Add GNU Cobol recipe

---
+++ build-gnucobol-master.sh
@@ -0,0 +1,194 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds GNU Cobol from sources.
+
+COBOL_TAR=gnucobol-3.1-rc1.tar.gz
+COBOL_DIR=gnucobol-3.1-rc1
+PKG_NAME=gnucobol
+
+###############################################################################
+
+CURR_DIR=$(pwd)
+function finish {
+    cd "$CURR_DIR" || exit 1
+}
+trap finish EXIT INT
+
+# Sets the number of make jobs if not set in environment
+: "${INSTX_JOBS:=2}"
+
+###############################################################################
+
+# Get the environment as needed. We can't export it because it includes arrays.
+if ! source ./setup-environ.sh
+then
+    echo "Failed to set environment"
+    exit 1
+fi
+
+if [[ -e "$INSTX_PKG_CACHE/$PKG_NAME" ]]; then
+    echo ""
+    echo "$PKG_NAME is already installed."
+    exit 0
+fi
+
+# The password should die when this subshell goes out of scope
+if [[ "$SUDO_PASSWORD_SET" != "yes" ]]; then
+    if ! source ./setup-password.sh
+    then
+        echo "Failed to process password"
+        exit 1
+    fi
+fi
+
+###############################################################################
+
+if ! ./build-cacert.sh
+then
+    echo "Failed to install CA Certs"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-iconv-gettext.sh
+then
+    echo "Failed to build iConv and GetText"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-gmp.sh
+then
+    echo "Failed to build GMP"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-libxml2.sh
+then
+    echo "Failed to build libxml2"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-bdb.sh
+then
+    echo "Failed to build Berkely DB"
+    exit 1
+fi
+
+###############################################################################
+
+echo
+echo "********** GNU Cobol **********"
+echo
+
+echo "**********************"
+echo "Downloading package"
+echo "**********************"
+
+if ! "$WGET" -q -O "$COBOL_TAR" --ca-certificate="$CA_ZOO" \
+     "https://alpha.gnu.org/gnu/gnucobol/$COBOL_TAR"
+then
+    echo "Failed to download GNU Cobol"
+    exit 1
+fi
+
+rm -rf "$COBOL_DIR" &>/dev/null
+gzip -d < "$COBOL_TAR" | tar xf -
+cd "$COBOL_DIR" || exit 1
+
+# Fix sys_lib_dlsearch_path_spec
+bash ../fix-configure.sh
+
+echo "**********************"
+echo "Configuring package"
+echo "**********************"
+
+    PKG_CONFIG_PATH="${INSTX_PKGCONFIG[*]}" \
+    CPPFLAGS="${INSTX_CPPFLAGS[*]}" \
+    CFLAGS="${INSTX_CFLAGS[*]}" \
+    CXXFLAGS="${INSTX_CXXFLAGS[*]}" \
+    LDFLAGS="${INSTX_LDFLAGS[*]}" \
+    LIBS="${INSTX_LIBS[*]}" \
+./configure \
+    --build="$AUTOCONF_BUILD" \
+    --prefix="$INSTX_PREFIX" \
+    --libdir="$INSTX_LIBDIR" \
+    --enable-shared \
+    --enable-static \
+    --with-gmp-include="$INSTX_PREFIX/include" \
+    --with-gmp-lib="$INSTX_LIBDIR"
+
+if [[ "$?" -ne 0 ]]; then
+    echo "Failed to configure GNU Cobol"
+    exit 1
+fi
+
+# Escape dollar sign for $ORIGIN in makefiles. Required so
+# $ORIGIN works in both configure tests and makefiles.
+bash ../fix-makefiles.sh
+
+echo "**********************"
+echo "Building package"
+echo "**********************"
+
+MAKE_FLAGS=("-j" "$INSTX_JOBS" "V=1")
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build GNU Cobol"
+    exit 1
+fi
+
+# Fix flags in *.pc files
+bash ../fix-pkgconfig.sh
+
+echo "**********************"
+echo "Testing package"
+echo "**********************"
+
+MAKE_FLAGS=("check" "V=1")
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to test GNU Cobol"
+    exit 1
+fi
+
+echo "**********************"
+echo "Installing package"
+echo "**********************"
+
+MAKE_FLAGS=("install")
+if [[ -n "$SUDO_PASSWORD" ]]; then
+    printf "%s\n" "$SUDO_PASSWORD" | sudo -E -S "${MAKE}" "${MAKE_FLAGS[@]}"
+else
+    "${MAKE}" "${MAKE_FLAGS[@]}"
+fi
+
+cd "$CURR_DIR" || exit 1
+
+# Set package status to installed. Delete the file to rebuild the package.
+touch "$INSTX_PKG_CACHE/$PKG_NAME"
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true; then
+
+    ARTIFACTS=("$COBOL_TAR" "$COBOL_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-gnucobol.sh 2>&1 | tee build-gnucobol.log
+    if [[ -e build-gnucobol.log ]]; then
+        rm -f build-gnucobol.log
+    fi
+fi
+
+exit 0
