1fe6a7edb76eec66f756d82d3af66c22e81c4510
Jeffrey Walton <noloader@gmail.com>
2020-07-16 04:56:32 -0400

    Bump ClamAV version

--- build-clamav.sh
+++ build-clamav.sh
@@ -4,8 +4,8 @@
 # This script builds ClamAV and its dependencies from sources.
 # Also see https://bugzilla.clamav.net/show_bug.cgi?id=11929
 
-CLAMAV_TAR=clamav-0.101.2.tar.gz
-CLAMAV_DIR=clamav-0.101.2
+CLAMAV_TAR=clamav-0.102.3.tar.gz
+CLAMAV_DIR=clamav-0.102.3
 
 ###############################################################################
 
