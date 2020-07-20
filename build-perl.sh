19888760c2408c5c9dfbb0bacc99a2bcaa06be0b
Jeffrey Walton <noloader@gmail.com>
2020-07-20 01:38:37 -0400

    Call fix-runpath.sh in Perl script

--- build-perl.sh
+++ build-perl.sh
@@ -61,6 +61,14 @@ fi
 
 ###############################################################################
 
+if ! ../build-patchelf.sh
+then
+    echo "Failed to build patchelf"
+    exit 1
+fi
+
+###############################################################################
+
 if ! ./build-zlib.sh
 then
     echo "Failed to build zLib"
@@ -207,6 +215,9 @@ fi
 
 # Fix flags in *.pc files
 bash ../fix-pkgconfig.sh
+
+# Fix runpaths
+bash ../fix-runpath.sh
 
 echo "**********************"
 echo "Testing package"
