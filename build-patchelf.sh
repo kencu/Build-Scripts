d73d635418832cebc3b72fb525c29c369a695c4e
Jeffrey Walton <noloader@gmail.com>
2020-07-20 01:32:46 -0400

    Update build-patchelf script

--- build-patchelf.sh
+++ build-patchelf.sh
@@ -149,6 +149,9 @@ fi
 
 cd "$CURR_DIR" || exit 1
 
+# Set package status to installed. Delete the file to rebuild the package.
+touch "$INSTX_PKG_CACHE/$PKG_NAME"
+
 ###############################################################################
 
 echo ""
