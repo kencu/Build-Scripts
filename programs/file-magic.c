98ffc2079117f69411f8e9e3f7dd555756de687f
Jeffrey Walton <noloader@gmail.com>
2020-07-21 20:11:18 -0400

    Update file-magic.c program

--- programs/file-magic.c
+++ programs/file-magic.c
@@ -1,3 +1,9 @@
+/* Written and placed in public domain by Jeffrey Walton */
+/* This program prints the first four bytes of a file.   */
+/* The program avoids shell portability problems by      */
+/* padding short reads with 0. The program always writes */
+/* four bytes to stdout. ELF is 7F454C46.                */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -5,17 +11,17 @@
 
 int main(int argc, char* argv[])
 {
-    int file = -1, ret = -1;
+    int fd = -1, ret = -1;
     unsigned char vals[4];
 
-    if (argc == 1)
+    if (argc == 1 || (argc == 2 && argv[1][0] == '-' && argv[1][1] == '\0'))
     {
-        file = STDIN_FILENO;
+        fd = STDIN_FILENO;
     }
     else if (argc == 2)
     {
-        file = open(argv[1], O_RDONLY);
-        if (file == -1)
+        fd = open(argv[1], O_RDONLY);
+        if (fd == -1)
         {
             ret = errno;
             goto done;
@@ -23,13 +29,13 @@ int main(int argc, char* argv[])
     }
     else
     {
-        fprintf(stderr, "file-magic <filename>\n");
+        fprintf(stderr, "file-magic <fdname>\n");
         ret = EBADF;
         goto done;
     }
 
     vals[0] = vals[1] = vals[2] = vals[3] = 0;
-    ret = read(file, vals, 4);
+    ret = read(fd, vals, 4);
 
     fprintf(stdout, "%02X%02X%02X%02X\n",
         vals[0], vals[1], vals[2], vals[3]);
@@ -38,8 +44,8 @@ int main(int argc, char* argv[])
 
 done:
 
-    if (file != -1)
-        close (file);
+    if (fd != -1)
+        close (fd);
 
     return ret;
 }
