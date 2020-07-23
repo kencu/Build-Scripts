d1b28627310a805f34366e0ecec45c8910c55edd
Jeffrey Walton <noloader@gmail.com>
2020-07-23 17:43:16 -0400

    Fix script on older Bash versions

--- build-pcsclite.sh
+++ build-pcsclite.sh
@@ -17,7 +17,7 @@ function finish {
 trap finish EXIT INT
 
 # Sets the number of pcsclite jobs if not set in environment
-: "${INSTX_JOBS:-2}"
+: "${INSTX_JOBS:=2}"
 
 ###############################################################################
 
