01ee8d758903cbdf1dc14b221202802828c6ae52
Jeffrey Walton <noloader@gmail.com>
2020-08-20 17:45:34 -0400

    Update INSTX_JOBS in recipes

--- build-perl.sh
+++ build-perl.sh
@@ -25,7 +25,7 @@ function finish {
 trap finish EXIT INT
 
 # Sets the number of make jobs if not set in environment
-: "${INSTX_JOBS:=2}"
+INSTX_JOBS="${INSTX_JOBS:-2}"
 
 ###############################################################################
 
