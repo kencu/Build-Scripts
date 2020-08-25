18c44410b3287c5d4082c68c7bb4939186f3f9d3
Jeffrey Walton <noloader@gmail.com>
2020-08-25 17:48:22 -0400

    Add dos2unix recipe

---
+++ build-dos2unix.sh
@@ -0,0 +1,150 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds dos2unix from sources.
+
+DOS2UNIX_TAR=dos2unix-7.4.1.tar.gz
+DOS2UNIX_DIR=dos2unix-7.4.1
+
+###############################################################################
+
+CURR_DIR=$(pwd)
+function finish {
+    cd "$CURR_DIR" || exit 1
+}
+trap finish EXIT INT
+
+INSTX_JOBS="${INSTX_JOBS:-2}"
+
+###############################################################################
+
+# Get the environment as needed. We can't export it because it includes arrays.
+if ! source ./setup-environ.sh
+then
+    echo "Failed to set environment"
+    exit 1
+fi
+
+# The password should die when this subshell goes out of scope
+if [[ "$SUDO_PASSWORD_DONE" != "yes" ]]; then
+    if ! source ./setup-password.sh
+    then
+        echo "Failed to process password"
+        exit 1
+    fi
+fi
+
+###############################################################################
+
+if ! ./build-cacert.sh
+then
+    echo "Failed to install CA Certs"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-iconv-gettext.sh
+then
+    echo "Failed to build iConv and GetText"
+    exit 1
+fi
+
+###############################################################################
+
+echo ""
+echo "========================================"
+echo "=============== dos2unix ==============="
+echo "========================================"
+
+echo ""
+echo "**********************"
+echo "Downloading package"
+echo "**********************"
+
+if ! "$WGET" -q -O "$DOS2UNIX_TAR" --ca-certificate="$LETS_ENCRYPT_ROOT" \
+     "https://waterlan.home.xs4all.nl/dos2unix/$DOS2UNIX_TAR"
+then
+    echo "Failed to download dos2unix"
+    exit 1
+fi
+
+rm -rf "$DOS2UNIX_DIR" &>/dev/null
+gzip -d < "$DOS2UNIX_TAR" | tar xf -
+cd "$DOS2UNIX_DIR" || exit 1
+
+# Patches are created with 'diff -u' from the pkg root directory.
+if [[ -e ../patch/dos2unix.patch ]]; then
+    patch -u -p0 < ../patch/dos2unix.patch
+    echo ""
+fi
+
+# Since we call the makefile directly, we need to escape dollar signs.
+CPPFLAGS=$(echo "${INSTX_CPPFLAGS[*]}" | sed 's/\$/\$\$/g')
+CFLAGS=$(echo "${INSTX_CFLAGS[*]}" | sed 's/\$/\$\$/g')
+CXXFLAGS=$(echo "${INSTX_CXXFLAGS[*]}" | sed 's/\$/\$\$/g')
+LDFLAGS=$(echo "${INSTX_LDFLAGS[*]}" | sed 's/\$/\$\$/g')
+LIBS="${INSTX_LIBS[*]}"
+
+MAKE_FLAGS=("-j" "$INSTX_JOBS")
+if ! CPPFLAGS="${CPPFLAGS}" \
+     CFLAGS="${CFLAGS}" \
+     CXXFLAGS="${CXXFLAGS}" \
+     LDFLAGS="${LDFLAGS}" \
+     LIBS="-lintl -liconv ${LIBS}" \
+     "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build dos2unix"
+    exit 1
+fi
+
+echo "**********************"
+echo "Testing package"
+echo "**********************"
+
+MAKE_FLAGS=("check" "V=1")
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo "**********************"
+    echo "Failed to test dos2unix"
+    echo "**********************"
+    exit 1
+fi
+
+echo "**********************"
+echo "Installing package"
+echo "**********************"
+
+MAKE_FLAGS=("install" "prefix=$INSTX_PREFIX" "libdir=$INSTX_LIBDIR")
+if [[ -n "$SUDO_PASSWORD" ]]; then
+    printf "%s\n" "$SUDO_PASSWORD" | sudo -E -S "${MAKE}" "${MAKE_FLAGS[@]}"
+else
+    "${MAKE}" "${MAKE_FLAGS[@]}"
+fi
+
+cd "$CURR_DIR" || exit 1
+
+###############################################################################
+
+echo ""
+echo "*****************************************************************************"
+echo "Please run Bash's 'hash -r' to update program cache in the current shell"
+echo "*****************************************************************************"
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true; then
+
+    ARTIFACTS=("$DOS2UNIX_TAR" "$DOS2UNIX_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-dos2unix.sh 2>&1 | tee build-dos2unix.log
+    if [[ -e build-dos2unix.log ]]; then
+        rm -f build-dos2unix.log
+    fi
+fi
+
+exit 0
