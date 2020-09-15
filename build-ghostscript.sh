2e4b8b5c451eb0d7409cf481825472ec2f1c4234
Jeffrey Walton <noloader@gmail.com>
2020-09-15 07:15:59 -0400

    Add GhostScript recipe

---
+++ build-ghostscript.sh
@@ -0,0 +1,184 @@
+#!/usr/bin/env bash
+
+# Written and placed in public domain by Jeffrey Walton
+# This script builds GhostScript from sources.
+
+GS_TAR=ghostpdl-9.53.1.tar.gz
+GS_DIR=ghostpdl-9.53.1
+
+###############################################################################
+
+CURR_DIR=$(pwd)
+function finish {
+    cd "$CURR_DIR" || exit 1
+}
+trap finish EXIT INT
+
+INSTX_JOBS="${INSTX_JOBS:-2}"
+
+###############################################################################
+
+# Get the environment as needed. We can't export it because it includes arrays.
+if ! source ./setup-environ.sh
+then
+    echo "Failed to set environment"
+    exit 1
+fi
+
+# The password should die when this subshell goes out of scope
+if [[ "$SUDO_PASSWORD_DONE" != "yes" ]]; then
+    if ! source ./setup-password.sh
+    then
+        echo "Failed to process password"
+        exit 1
+    fi
+fi
+
+###############################################################################
+
+if ! ./build-cacert.sh
+then
+    echo "Failed to install CA Certs"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-iconv-gettext.sh
+then
+    echo "Failed to build iConv and GetText"
+    exit 1
+fi
+
+###############################################################################
+
+if ! ./build-idn2.sh
+then
+    echo "Failed to build IDN2"
+    exit 1
+fi
+
+###############################################################################
+
+echo ""
+echo "========================================"
+echo "============== GhostScript ============="
+echo "========================================"
+
+echo ""
+echo "**********************"
+echo "Downloading package"
+echo "**********************"
+
+if ! "$WGET" -q -O "$GS_TAR" --ca-certificate="$LETS_ENCRYPT_ROOT" \
+     "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9531/$GS_TAR"
+then
+    echo "Failed to download GhostScript"
+    exit 1
+fi
+
+rm -rf "$GS_DIR" &>/dev/null
+gzip -d < "$GS_TAR" | tar xf -
+cd "$GS_DIR" || exit 1
+
+# Patches are created with 'diff -u' from the pkg root directory.
+if [[ -e ../patch/gs.patch ]]; then
+    patch -u -p0 < ../patch/gs.patch
+    echo ""
+fi
+
+exit 1
+
+# Fix sys_lib_dlsearch_path_spec
+bash ../fix-configure.sh
+
+echo "**********************"
+echo "Configuring package"
+echo "**********************"
+
+    PKG_CONFIG_PATH="${INSTX_PKGCONFIG[*]}" \
+    CPPFLAGS="${INSTX_CPPFLAGS[*]}" \
+    ASFLAGS="${INSTX_ASFLAGS[*]}" \
+    CFLAGS="${INSTX_CFLAGS[*]}" \
+    CXXFLAGS="${INSTX_CXXFLAGS[*]}" \
+    LDFLAGS="${INSTX_LDFLAGS[*]}" \
+    LIBS="${INSTX_LIBS[*]}" \
+./configure \
+    --build="$AUTOCONF_BUILD" \
+    --prefix="$INSTX_PREFIX" \
+    --libdir="$INSTX_LIBDIR" \
+    --with-libiconv=native
+
+if [[ "$?" -ne 0 ]]; then
+    echo "Failed to configure GhostScript"
+    exit 1
+fi
+
+# Escape dollar sign for $ORIGIN in makefiles. Required so
+# $ORIGIN works in both configure tests and makefiles.
+bash ../fix-makefiles.sh
+
+echo "**********************"
+echo "Building package"
+echo "**********************"
+
+MAKE_FLAGS=("MAKEINFO=true" "-j" "$INSTX_JOBS" "V=1")
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo "Failed to build GhostScript"
+    exit 1
+fi
+
+# Fix flags in *.pc files
+bash ../fix-pkgconfig.sh
+
+echo "**********************"
+echo "Testing package"
+echo "**********************"
+
+MAKE_FLAGS=("check" "-k" "V=1")
+if ! "${MAKE}" "${MAKE_FLAGS[@]}"
+then
+    echo "**********************"
+    echo "Failed to test GhostScript"
+    echo "**********************"
+    exit 1
+fi
+
+echo "**********************"
+echo "Installing package"
+echo "**********************"
+
+MAKE_FLAGS=("install")
+if [[ -n "$SUDO_PASSWORD" ]]; then
+    printf "%s\n" "$SUDO_PASSWORD" | sudo -E -S "${MAKE}" "${MAKE_FLAGS[@]}"
+else
+    "${MAKE}" "${MAKE_FLAGS[@]}"
+fi
+
+cd "$CURR_DIR" || exit 1
+
+###############################################################################
+
+echo ""
+echo "*****************************************************************************"
+echo "Please run Bash's 'hash -r' to update program cache in the current shell"
+echo "*****************************************************************************"
+
+###############################################################################
+
+# Set to false to retain artifacts
+if true; then
+
+    ARTIFACTS=("$GS_TAR" "$GS_DIR")
+    for artifact in "${ARTIFACTS[@]}"; do
+        rm -rf "$artifact"
+    done
+
+    # ./build-gs.sh 2>&1 | tee build-gs.log
+    if [[ -e build-gs.log ]]; then
+        rm -f build-gs.log
+    fi
+fi
+
+exit 0
